799228492f4f024458bb6b9119f73dcedc301ba3
prjpet <peter.prjevara@gmail.com>
2016-05-23 14:08:32 +0100

    Update StockManager.py
    Use constants from Stock class for better readability.

--- lib/StockManager.py
+++ lib/StockManager.py
@@ -3,10 +3,10 @@ from lib.Stock import Stock
 class StockManager:
 
     def __init__(self):
-        self.listOfStocks = {"TEA": Stock("TEA", "Common", 0, 0, 100),
-                             "POP": Stock("POP", "Common", 8, 0, 100),
-                             "ALE": Stock("ALE", "Common", 23, 0, 60),
-                             "GIN": Stock("GIN", "Preferred", 8, 0.02, 100),
-                             "JOE": Stock("JOE", "Common", 13, 0, 200)
+        self.listOfStocks = {"TEA": Stock("TEA", Stock.COMMON, 0, 0, 100),
+                             "POP": Stock("POP", Stock.COMMON, 8, 0, 100),
+                             "ALE": Stock("ALE", Stock.COMMON, 23, 0, 60),
+                             "GIN": Stock("GIN", Stock.PREFERRED, 8, 0.02, 100),
+                             "JOE": Stock("JOE", Stock.COMMON, 13, 0, 200)
                              }
         
